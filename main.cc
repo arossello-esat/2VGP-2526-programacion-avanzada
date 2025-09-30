0f1961ce8222139d71d241348f340c694355fce0
Arnau Rosselló <arnau.rossello@external-outfit7.com>
2025-09-30 11:26:20 +0200

    Co-authored-by: czagerf <czagerf@users.noreply.github.com> Co-authored-by: raqgironape <raqgironape@users.noreply.github.com> Co-authored-by: corteganaal <corteganaal@users.noreply.github.com> Co-authored-by: AdamLioubavina <Apl512@users.noreply.github.com> Co-authored-by: moramoel <moramoel@users.noreply.github.com> Co-authored-by: baich-cloud <baich-cloud@users.noreply.github.com> Co-authored-by: AlvaroGS8 <AlvaroGS8@users.noreply.github.com> Co-authored-by: JoseLuis-VersionEsa <JoseLuis-VersionEsa@users.noreply.github.com> Co-authored-by: JonathanESAT <JonathanESAT@users.noreply.github.com> Co-authored-by: reco <JorgeLS1@users.noreply.github.com> Co-authored-by: AnnaShakCh <AnnaShakCh@users.noreply.github.com> Co-authored-by: guerrerofe <guerrerofe@users.noreply.github.com> Co-authored-by: ferrandola <DevolvedmeElPendrive@users.noreply.github.com> Co-authored-by: floresru <floresru@users.noreply.github.com> Co-authored-by: leandrezpau <leandrezpau@users.noreply.github.com> Co-authored-by: golpeme <golpeme@users.noreply.github.com> Co-authored-by: garcesmatrucha67 <garcesmatrucha67@users.noreply.github.com> Co-authored-by: Pau Alvarez <Pau-Alvarez@users.noreply.github.com> Co-authored-by: Jacopazo <JoackOpeace@users.noreply.github.com>

---
+++ main.cc
@@ -0,0 +1,19 @@
+
+int main(){
+  Parcheesi parchis{4};  //Instancia
+
+  int entero_construido_con_parametros{1};
+
+  srand(time(NULL));
+
+ 
+  bool win = false;
+  do{
+    
+  }while(!win);
+
+  
+  return 0;
+}
+
+    

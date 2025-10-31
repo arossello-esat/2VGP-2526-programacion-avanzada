279f0d6154c759f8241fd30bac25f0d15d452493
Arnau Rosselló <arossello@esat.es>
2025-10-31 11:31:57 +0100

    Share libraries with others

--- parcheesi/premake5.lua
+++ parcheesi/premake5.lua
@@ -4,14 +4,33 @@ workspace "Parcheesi"
    configurations { "Debug", "Release" }
    startproject "Parcheesi"
    location "build"
+   libdirs {"."}
+
+project "PlayerYVuestroNombre"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++17"
+   files { "VuestroPlayer.h", "VuestroPlayer.cc"}
+
+project "BoardYVuestroNombre"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++17"
+
+   files { "VuestroParcheesi.cc", "VuestroParcheesi.h" }
 
 project "Parcheesi"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
+   links {"ParcheesiPlayer","ParcheesiBoard", "PlayerPaco.lib"}
 
    files
    {
-      "**.h",
-      "**.cc"
+      "game.h",
+      "iparcheesi.h",
+      "mock_parcheesi.h",
+
+      "game.cc",
+      "main.cc"
    }
